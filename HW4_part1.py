a53905dd73a5c9b8b6598fc412d847d2a90f8e9e
Reid Reininger <charles.reininger@wsu.edu>
2018-10-28 09:49:15 -0700

    Implement dictPop() and dictPush()

--- HW4_part1.py
+++ HW4_part1.py
@@ -3,9 +3,16 @@
 #desc: (intended for Unix/Linux)
 
 opstack = []
+dictstack = []
 
 def opPop():
     return opstack.pop()
 
 def opPush(value):
     return opstack.append(value)
+
+def dictPop():
+    return dictstack.pop()
+
+def dictPush():
+    return dictstack.append({})
