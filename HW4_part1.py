e76c6dd9d217ade7bed197f8b470c0797ee81e5f
Reid Reininger <charles.reininger@wsu.edu>
2018-10-28 10:24:29 -0700

    Update documentation

--- HW4_part1.py
+++ HW4_part1.py
@@ -2,25 +2,34 @@
 #date: 10/24/18
 #desc: (intended for Unix/Linux)
 
+#globals
 opstack = []
 dictstack = [{}]
 
+#operand stack operators
 def opPop():
+    """Pop opstack."""
     return opstack.pop()
 
 def opPush(value):
+    """Push value onto opstack."""
     return opstack.append(value)
 
+#dictionary stack operators
 def dictPop():
+    """Pop dictstack."""
     return dictstack.pop()
 
 def dictPush():
+    """Push empty dict onto dictstack."""
     return dictstack.append({})
 
 def define(name, value):
+    """Add name:value pair to top dict in dictstack."""
     dictstack[-1][name] = value
 
 def lookup(name):
+    """Return most recently defined value for name."""
     for x in reversed(dictstack):
         for key in x:
             if key == name:
